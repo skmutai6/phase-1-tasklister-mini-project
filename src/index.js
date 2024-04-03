94fc672f8a58f6c608036775ee64677ef4d8ef9e
skmutai6 <skmutai6@gmail.com>
2024-04-03 18:23:42 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent default behavior
+    const taskName = document.getElementById('new-task-description');
+    const taskDescription = taskName.value;
+
+    // new task 
+    const newTask = document.createElement('li');
+    newTask.textContent = taskDescription;
+
+    // list your tasks
+    taskList.appendChild(newTask);
+
+  });
 });
+
+
+
